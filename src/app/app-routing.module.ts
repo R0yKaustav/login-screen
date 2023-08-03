75e28c0fce188f54eb1a6fed2dba0d1bfc9fe08f
R0yKaustav <141066452+R0yKaustav@users.noreply.github.com>
2023-08-03 10:10:52 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component, HostBinding, OnInit } from '@angular/core';
+/**
+ *  Buttons Page for Design Framework
+ */
+@Component({
+  selector: 'cgi-buttons-style',
+  templateUrl: './buttons-style.component.html',
+  styleUrls: ['./buttons-style.component.scss'],
+})
+export class ButtonsStyleComponent implements OnInit {
+  /**
+   * HostBinding ( Decorator allows you to set the properties of the host element from the directive class.)
+   */
+  @HostBinding('class.cgi-buttons-style')
+  isClassContainer: boolean;
+  /**
+   * Constructor
+   */
+  constructor() {
+    this.isClassContainer = true;
+  }
+  /**
+   * Life cycle hook for when the component is first initiated.
+   */
+  ngOnInit() {}
+}
 
 const routes: Routes = [];
 
